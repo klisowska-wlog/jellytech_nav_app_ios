1f5c28cd28186e23373e8dc76abe7c1f9afcbc3e
klisowska-wlog <86469730+klisowska-wlog@users.noreply.github.com>
2022-11-20 11:48:30 +0100

    Initial Commit

---
+++ Jelly_Navigation_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jelly_Navigation_App
+//
+//  Created by Karolina Lisowska on 11/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
