d8da68d721536c5b34418f235ec10663da03fc24
Karolina Lisowska <86469730+klisowska-wlog@users.noreply.github.com>
2022-11-27 14:12:54 +0100

    Release v1.0.0 - Initial iOS navigation app (#1)
    * Initial Commit
    
    * Initial navigation app
    
    * Refactor and UI improvements
    
    * Added navigation within app

---
+++ Jelly_Navigation_App/Jelly_Navigation_App.swift
@@ -0,0 +1,19 @@
+//
+//  Jelly_Navigation_App.swift
+//  Jelly_Navigation_App
+//
+//  Created by Karolina Lisowska on 11/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Jelly_Navigation_App: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView()
+        }
+    }
+}
+
+
